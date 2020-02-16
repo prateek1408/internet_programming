cc4b38a8589a982cd3b62137dde61a3dd83e1842
Prateek Budhiraja <36406919+prateek1408@users.noreply.github.com>
2020-02-16 22:47:33 +0530

    Update bill.c

--- bill.c
+++ bill.c
@@ -26,8 +26,6 @@ void main(){
   final_price = price + tax;
   printf("\nThank you for visiting!\nYou can collect your bill now....");
   
- /* printf("/nPrice : %f ", price);
-  printf("/nFinal price : %f", final_price);*/
   fprintf(fptr,"<center><b>Customer Id. :  %d</b></center><br>", idno);
   fprintf(fptr,"<table style=\"width:100%\" border = 1 cellpadding = 20 align = \"center\"><tr><th>Name:</th><td>%s</td></tr><tr><th rowspan=\"2\">Amount:</th><td>%.3f</td></tr><tr><td>+%.2f(tax)</td></tr>", name, price, tax); 
   fprintf(fptr,"<tr><th>Total:</th><td>%.3f</td></tr></table></body></html>", final_price);
